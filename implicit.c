da1e8ea2d3963a70c51703bdc983dfb74b683857
faruk <faruk.isiker@tekhnelogos.com>
2022-03-22 15:11:10 +0300

    second option

--- implicit.c
+++ implicit.c
@@ -1,31 +1,34 @@
+
+// A simple C program to show function pointers as parameter
 #include <stdio.h>
 
-int add(int a, int b)
-{
-	printf("x = %d y = %d \t x+y = %d\n",a,b,a+b);
-	return a+b;
+int multiply(int a, int b){
+    printf("x = %d y = %d \t x*y = ",a,b);
+    return a*b;
 }
-int subtract(int a, int b)
-{
-	printf("x = %d y = %d \t x-y = %d\n",a,b,a-b);
-	return a-b;
+int add(int a, int b){
+    printf("x = %d y = %d \t x+y = ",a,b);
+    return a+b;
 }
-int multiply(int a, int b)
-{
-	printf("x = %d y = %d \t x*y = %d\n",a,b,a*b);
-	return a*b;
+int substract(int a, int b){
+    printf("x = %d y = %d \t x-y = ",a,b);
+    return a-b;
 }
 
+void calculater(int (*fun)(),int a, int b)
+{
+    printf("%d\n",(fun)(a,b));
+}
+
+// A function that receives a simple function
+// as parameter and calls the function
+  
 int main()
 {
-	// fun_ptr_arr is an array of function pointers
-	int (*fun_ptr_arr[])(int, int) = {add, subtract, multiply};
-	int a = 15, b = 10;
-    unsigned int c;
-	int x = (*fun_ptr_arr[0])(a, b);
-	x = (*fun_ptr_arr[1])(a, b);
-	x = (*fun_ptr_arr[2])(a, b);
-    scanf("%d",&c);
-    x = (*fun_ptr_arr[c])(a, b);
-	return 0;
+    int x = 4, y = 2;
+    calculater(substract,x,y);
+    calculater(add,x,y);
+    calculater(multiply,x,y);
+    
+    return 0;
 }
